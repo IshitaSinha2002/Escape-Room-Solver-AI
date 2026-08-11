cad15ff74b8918fd44b2c38d62dd1425cf8a348e
Ishita Sinha <ishitasinha16@gmail.com>
2026-08-12 00:29:49 +0530

    Adding state

---
+++ app/state.py
@@ -0,0 +1,10 @@
+from typing import TypedDict, List
+
+class EscapeRoomState(TypedDict):
+    room_state: str
+    clues: List[str]
+    inventory: List[str]
+    observations: List[str]
+    current_actions: str
+    solution: str
+    is_solved: bool
