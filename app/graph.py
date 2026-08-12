5dbfadb9a73f0a7dbac6ff179411004ab917bc20
Ishita Sinha <ishitasinha16@gmail.com>
2026-08-12 12:04:01 +0530

    Adding check solved

--- app/graph.py
+++ app/graph.py
@@ -1,16 +1,23 @@
 from langgraph.graph import StateGraph, START, END
 
 from app.state import EscapeRoomState
-from app.nodes import analyze_room, execute_action
+from app.nodes import (analyze_room, execute_action, check_solved)
+
+def route_after_check(state: EscapeRoomState):
+    if state["is_solved"]:
+        return "end"
+    return "continue"
 
 def build_graph():
     graph = StateGraph(EscapeRoomState)
 
     graph.add_node("analyze_room", analyze_room)
     graph.add_node("execute_action", execute_action)
+    graph.add_node("check_solved", check_solved)
 
     graph.add_edge(START, "analyze_room")
     graph.add_edge("analyze_room", "execute_action")
-    graph.add_edge("execute_action", END)
+    graph.add_edge("execute_action", "check_solved")
+    graph.add_conditional_edges("check_solved", route_after_check, {"continue": "analyze_room","end": END})
 
     return graph.compile()
--- app/nodes.py
+++ app/nodes.py
@@ -35,9 +35,49 @@ def analyze_room(state: EscapeRoomState):
     }
 
 def execute_action(state: EscapeRoomState):
-    action = state["current_action"]
-    action_result = f"Action Performed: {action}"
+    action = state["current_action"].lower()
+
+    inventory = state["inventory"].copy()
+    clues = state["clues"].copy()
+
+    if "315" in action and "drawer" in action:
+        action_result = (
+            "The code 315 is correct. "
+            "The wooden drawer opens. "
+            "Inside the drawer, you find a silver key."
+        )
+
+        if "silver key" not in inventory:
+            inventory.append("silver key")
+
+        clues.append("The silver key may unlock the main door.")
+
+    elif "silver key" in action and "door" in action:
+        action_result = (
+            "You use the silver key on the main door. "
+            "The door unlocks."
+        )
+
+    else:
+        action_result = (
+            f"You attempt to perform the following action: {state['current_action']}. "
+            "Nothing significant happens."
+        )
+
     return {
+        "inventory": inventory,
+        "clues": clues,
         "action_result": action_result,
         "observations": state["observations"] + [action_result]
+    }
+
+def check_solved(state: EscapeRoomState):
+    if "door unlocks" in state["action_result"].lower():
+        return {
+            "is_solved": True,
+            "solution": "Use the silver key to unlock the main door."
+        }
+
+    return {
+        "is_solved": False
     }
